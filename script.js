3c7dea2668b8d2a2af7f56c61189842dc401eaaf
Afza Azzindani <109005550+azzindani@users.noreply.github.com>
2024-09-28 07:18:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.getElementById('toggleSidebar').addEventListener('click', function () {
     const sidebar = document.getElementById('sidebar');
-    const mainContent = document.getElementById('main-content');
     sidebar.classList.toggle('collapsed');
 });
